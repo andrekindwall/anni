aa55041f1f0f235da739fd7000ef1258a8148506
drewi <andre.kindwall@gmail.com>
2013-01-26 21:58:05 +0100

    Added a character to the scene

--- src/com/lolbro/anni/views/MainActivity.java
+++ src/com/lolbro/anni/views/MainActivity.java
@@ -1,29 +1,52 @@
 package com.lolbro.anni.views;
 
 import org.andengine.engine.camera.Camera;
-import org.andengine.engine.camera.CameraFactory;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
-import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.AutoParallaxBackground;
 import org.andengine.entity.scene.background.ParallaxBackground.ParallaxEntity;
+import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.util.FPSLogger;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-public class MainActivity extends SimpleBaseGameActivity {
+import android.hardware.SensorManager;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
+public class MainActivity extends SimpleBaseGameActivity implements IOnSceneTouchListener {
 	
 	private static final int CAMERA_WIDTH = 720;
 	private static final int CAMERA_HEIGHT = 480;
 	
-	private BitmapTextureAtlas mBackgroundTexture;
+	private static final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
+	
+	private BitmapTextureAtlas mBackgroundTextureAtlas;
 	private ITextureRegion mBackgroundLayerBack;
+	
+	private BitmapTextureAtlas mCharactersTextureAtlas;
+	private TiledTextureRegion mPlayerTextureRegion;
+	
+	private Scene mScene;
+
+	private PhysicsWorld mPhysicsWorld;
 	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
@@ -38,22 +61,63 @@ public class MainActivity extends SimpleBaseGameActivity {
 	protected void onCreateResources() {
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 		
-		mBackgroundTexture = new BitmapTextureAtlas(this.getTextureManager(), 1024, 1024);
-		mBackgroundLayerBack = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBackgroundTexture, this, "scene_background.png", 0, 188);
-		mBackgroundTexture.load();
+		//Create atlas for background
+		mBackgroundTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 1024, 512);
+		mBackgroundLayerBack = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBackgroundTextureAtlas, this, "scene_background.png", 0, 0);
+		mBackgroundTextureAtlas.load();
+		
+		//Create texture atlas for mobs
+		mCharactersTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 32, 64, TextureOptions.BILINEAR);
+		mPlayerTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(mCharactersTextureAtlas, this, "player.png", 0, 0, 1, 1);
+		mCharactersTextureAtlas.load();
 	}
 
 	@Override
 	protected Scene onCreateScene() {
 		mEngine.registerUpdateHandler(new FPSLogger());
 		
-		final Scene scene = new Scene();
-		final AutoParallaxBackground autoParallaxBackground = new AutoParallaxBackground(0, 0, 0, 5);
-		final VertexBufferObjectManager vertexBufferObjectManager = this.getVertexBufferObjectManager();
-		autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(0.0f, new Sprite(0, CAMERA_HEIGHT - mBackgroundLayerBack.getHeight(), mBackgroundLayerBack, vertexBufferObjectManager)));
-		scene.setBackground(autoParallaxBackground);
+		final VertexBufferObjectManager vertexBufferObjectManager = getVertexBufferObjectManager();
 		
-		return scene;
+		mScene = new Scene();
+		
+		//Create background
+		//Right now Parallax background is meaningless because we don't have a moving background
+		final AutoParallaxBackground autoParallaxBackground = new AutoParallaxBackground(0, 0, 0, 5);
+		autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(0.0f, new Sprite(0, CAMERA_HEIGHT - mBackgroundLayerBack.getHeight(), mBackgroundLayerBack, vertexBufferObjectManager)));
+		mScene.setBackground(autoParallaxBackground);
+		
+		//Create physic world and set gravity
+		mPhysicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
+		
+		//Set the properties for out walls
+		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
+		
+		//Create a shape for the ground
+		final Rectangle ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2, vertexBufferObjectManager);
+		//Create physics/collision for the ground
+		PhysicsFactory.createBoxBody(mPhysicsWorld, ground, BodyType.StaticBody, wallFixtureDef);
+		//Make the ground visible in the scene
+//		mScene.attachChild(ground);
+		
+		//Create a sprite for our player
+		AnimatedSprite playerBody = new AnimatedSprite(CAMERA_WIDTH/2, CAMERA_HEIGHT/2, mPlayerTextureRegion, this.getVertexBufferObjectManager());
+		//Create physics for the player. We use BoxBody for now
+		Body body = PhysicsFactory.createBoxBody(mPhysicsWorld, playerBody, BodyType.DynamicBody, FIXTURE_DEF);
+		//Place the player in the scene
+		mScene.attachChild(playerBody);
+		
+		
+		mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(playerBody, body, true, true));
+		
+		mScene.registerUpdateHandler(mPhysicsWorld);
+		
+		return mScene;
+	}
+
+	@Override
+	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
